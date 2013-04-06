abda22b8727ed9f0b7ae4dc9dfce00da83bbf7bc
sasaki takeru <sasaki.takeru@gmail.com>
2013-04-07 01:52:54 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peerjsdemo::Application.config.secret_token = '12808814941853b584f84aa5fe120b44a17cb917f15406096c4cbd284a3e41e413aa9b86e0f14ad76c479849092eb5cb63ff8652a2ad1338cc9725883ceeb78a'
